53430f1381bf372fb5e9a73c72f894e3b7d7622b
dnt.201 <89123004+dnt201@users.noreply.github.com>
2023-03-28 22:03:42 +0700

    add toolkit

--- craco.config.js
+++ craco.config.js
@@ -3,7 +3,12 @@ const path = require('path');
 module.exports = {
   webpack: {
     alias: {
-      "@pages": path.resolve(__dirname, 'src/pages/')
+      "@api": path.resolve(__dirname, 'src/api/'),
+      "@app": path.resolve(__dirname, 'src/app/'),
+      "@pages": path.resolve(__dirname, 'src/pages/'),
+      "@redux": path.resolve(__dirname, 'src/redux/'),
+      "@router": path.resolve(__dirname, 'src/router/')
+
     }
   }
 };
--- src/pages/MyFiles/index.tsx
+++ src/pages/MyFiles/index.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
 
+import React, { useState } from 'react'
+
+import { useAppSelector, useAppDispatch } from 'app/hooks'
+
+import { decrement, increment } from '@redux/counterSlice'
+
+export function Counter() {
+  // The `state` arg is correctly typed as `RootState` already
+  const count = useAppSelector((state) => state.counter.value)
+  const dispatch = useAppDispatch()
+
+  // omit rendering logic
+}
 const MyFiles = () => {
+  const count = useAppSelector((state) => state.counter.value)
+  const dispatch = useAppDispatch();
+
   return (
-    <div className=''>MyFiles</div>
+    <div>
+      <div className=''>MyFiles {count}</div>
+      <button onClick={()=> dispatch(increment())}>up</button>
+      <button onClick={()=> dispatch(decrement())}>down</button>
+    </div>
   )
 }
 
